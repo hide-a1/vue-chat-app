a6fe41c19393afca2a01f5210c4b00b4b71dfe02
hide-a1 <hide210g@gmail.com>
2021-07-28 12:47:28 +0900

    chat

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// Vue.config.productionTip = false;
+
+// Initialize Firebase
+
+createApp(App).mount("#app");
